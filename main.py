6b4f51c046c6de30a81073d1bc3f4898edef1e31
selinmarvank <32520417+selinmarvank@users.noreply.github.com>
2025-08-21 20:53:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello, Google Cloud Run!"
+@app.route('/')
+def hello():
+    return "Hello, Cloud Run!"
 
 if __name__ == "__main__":
-    # Cloud Run expects the app to run on 0.0.0.0 and port 8080
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
